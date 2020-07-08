65e0c2da1b7bc53428f9e46eae0725995bf51668
Klaus Zipfel <zipfel@iqo.uni-hannover.de>
2020-07-08 15:42:23 +0200

    Added settings for my own mouse

--- leetmouse.c
+++ leetmouse.c
@@ -29,22 +29,26 @@
  * If you don't know what yours is, follow this link:
  * https://wiki.archlinux.org/index.php/Mouse_polling_rate
  */
-#define POLLING_RATE 125
+//TODO: Actually derive "polling rate" live from mouse input events, as it is done in InterAcceel.
+#define POLLING_RATE 1000
 
 /*
  * This should be your desired acceleration. It needs to end with an f.
  * For example, setting this to "0.1f" should be equal to
  * cl_mouseaccel 0.1 in Quake.
  */
-#define ACCELERATION 0.2f
 
-#define SENSITIVITY 1.0f
-#define SENS_CAP 0.0f
+// These settings basically emulate Windows' Enhanced Pointer Precision for my 7200 DPI mouse
+
+#define ACCELERATION 0.26f
+
+#define SENSITIVITY 0.85f
+#define SENS_CAP 4.0f
 #define OFFSET 0.0f
-#define PRE_SCALE_X 1.0f
-#define PRE_SCALE_Y 1.0f
-#define POST_SCALE_X 1.0f
-#define POST_SCALE_Y 1.0f
+#define PRE_SCALE_X 0.0555555f
+#define PRE_SCALE_Y 0.0555555f
+#define POST_SCALE_X 0.4f
+#define POST_SCALE_Y 0.4f
 #define SPEED_CAP 0.0f
 
 
