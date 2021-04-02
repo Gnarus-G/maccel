cb0d6181594724fd94cba46261e2ebb11667f24c
Klaus Zipfel <klaus@zipfel.family>
2021-04-03 01:52:28 +0200

    Refactoring

--- accel.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ACCEL_H
-#define _ACCEL_H
-
-int accelerate(int* x, int* y, int* wheel);
-
-#endif /* _ACCEL_H */
--- util.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UTIL_H
-#define _UTIL_H
-
-void atof(const char* str, int len, float* result);
-int Leet_round(float x);
-void Q_sqrt(float* number);
-
-#endif  //_UTIL_H
