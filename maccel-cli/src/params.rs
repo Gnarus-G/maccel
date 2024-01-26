41aa524a58d1f0de10b7e1b766a2eb3613487e38
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-01-26 17:33:15 -0500

    feat: a way to remember the parameter's value on reboot

--- maccel-cli/src/params.rs
+++ maccel-cli/src/params.rs
@@ -19,24 +19,41 @@ pub enum Param {
 
 impl Param {
     pub fn set(&self, value: f32) -> anyhow::Result<()> {
-        let param_path = get_param_path(self)?;
         let value = fixedpt(value);
 
-        let mut file =
-            File::create(param_path).context("failed to open the parameter's file for writing")?;
+        let mut file = File::create(self.path()?)
+            .context("failed to open the parameter's file for writing")?;
 
         use std::io::Write;
 
-        write!(file, "{}", value.0).context("failed to write to parameter file")?;
+        let value = value.0;
+        write!(file, "{}", value).context("failed to write to parameter file")?;
+        self.save_reset_script(value)?;
 
         Ok(())
     }
 
-    pub fn get(&self) -> anyhow::Result<Fixedpt> {
-        let param_path = get_param_path(self)?;
+    fn save_reset_script(&self, value: i32) -> anyhow::Result<()> {
+        let script_dir = "/var/lib/maccel";
+        if !Path::new(script_dir).exists() {
+            std::fs::create_dir(script_dir).context(format!("failed create directory: {}", script_dir))
+                .context("failed to create the directory where we'd save the parameter value to apply on reboot")?;
+        }
+        std::fs::write(
+            format!("{script_dir}/set_last_{}_value.sh", self.name()),
+            format!(
+                "echo {} > /sys/module/maccel/parameters/{};",
+                value,
+                self.name()
+            ),
+        )
+        .context("failed to write reset script")?;
+        Ok(())
+    }
 
+    pub fn get(&self) -> anyhow::Result<Fixedpt> {
         let mut file =
-            File::open(param_path).context("failed to open the parameter's file for reading: this shouldn't happen unless the maccel kernel module is not installed.")?;
+            File::open(self.path()?).context("failed to open the parameter's file for reading: this shouldn't happen unless the maccel kernel module is not installed.")?;
 
         let mut buf = String::new();
 
@@ -61,22 +78,26 @@ impl Param {
     pub fn display_name(&self) -> String {
         return format!("{:?}", self);
     }
-}
 
-fn get_param_path(name: &Param) -> anyhow::Result<PathBuf> {
-    let param_name = match name {
-        Param::Accel => "ACCEL",
-        Param::Offset => "OFFSET",
-        Param::OutputCap => "OUTPUT_CAP",
-    };
-
-    let params_path = Path::new(SYS_MODULE_PATH)
-        .join("parameters")
-        .join(param_name);
-
-    if !params_path.exists() {
-        return Err(anyhow!("no such parameter {:?}", name));
+    /// The cannonical name of parameter, exactly what can be read from /sys/module/maccel/parameters
+    fn name(&self) -> &'static str {
+        let param_name = match self {
+            Param::Accel => "ACCEL",
+            Param::Offset => "OFFSET",
+            Param::OutputCap => "OUTPUT_CAP",
+        };
+        return param_name;
     }
 
-    return Ok(params_path);
+    fn path(&self) -> anyhow::Result<PathBuf> {
+        let params_path = Path::new(SYS_MODULE_PATH)
+            .join("parameters")
+            .join(self.name());
+
+        if !params_path.exists() {
+            return Err(anyhow!("no such parameter {:?}", self));
+        }
+
+        return Ok(params_path);
+    }
 }
