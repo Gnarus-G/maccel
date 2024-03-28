5ac677dd6560adab5a7c2405e85801d9614d2a84
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-27 22:25:00 -0400

    fix: compiler error when DEBUG is not defined

--- driver/dbg.h
+++ driver/dbg.h
@@ -1,33 +1,27 @@
 #ifdef DEBUG
-#ifdef __KERNEL__
-#include <linux/printk.h>
-#else
-#include <stdio.h>
-#endif
 #define DEBUG_TEST 1
 #else
-#ifndef __KERNEL__
-#include <stdio.h>
-#endif
 #define DEBUG_TEST 0
 #endif
 
 #ifdef __KERNEL__
+#include <linux/printk.h>
 #define dbg(fmt, ...) dbg_k(fmt, __VA_ARGS__)
 #else
+#include <stdio.h>
 #define dbg(fmt, ...) dbg_std(fmt, __VA_ARGS__)
 #endif
 
 #ifdef __KERNEL__
 #define dbg_k(fmt, ...)                                                        \
-  _Pragma(" clang diagnostic push")                                            \
+  _Pragma("clang diagnostic push")                                             \
       _Pragma("clang diagnostic ignored \"-Wstatic-local-in-inline\"") do {    \
     if (DEBUG_TEST)                                                            \
       printk(KERN_INFO "%s:%d:%s(): " #fmt "\n", __FILE__, __LINE__, __func__, \
              __VA_ARGS__);                                                     \
   }                                                                            \
   while (0)                                                                    \
-  _Pragma(" clang diagnostic pop")
+  _Pragma("clang diagnostic pop")
 #endif
 
 #ifndef __KERNEL__
