6f37cf58455f25b6a8948c8363cb98fd569de57e
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-06-23 22:30:39 -0400

    feat: update install script to warn about the need for the user to update their parameters value with the next version of the driver

--- install.sh
+++ install.sh
@@ -8,6 +8,47 @@ setup_dirs() {
   git clone --depth 1 https://github.com/Gnarus-G/maccel.git .
 }
 
+version_update_warning(){
+  print_bold() {
+    printf "\e[1m$1\e[22m"
+  }
+
+  print_yellow() {
+    printf "\e[33m$1\e[0m"
+  }
+
+  if ! which maccel &> /dev/null; then
+    print_yellow "BAD"
+    return
+  fi
+
+  CURR_VERSION=$(maccel -V | awk '{ print $2 }')
+
+  if [[ "$CURR_VERSION" < "0.1.3" ]]; then
+    print_yellow $(print_bold "ATTENTION!")
+    printf "\n\n"
+
+    print_yellow "The precision for the processed values has been updated since version '$CURR_VERSION';\n"
+    EMPHASIS=$(print_bold "MUST re-enter your parameter values in maccel")
+    print_yellow "This means that you $EMPHASIS.\n"
+    print_yellow "Otherwise your curve and mouse movement won't work as expected.\n"
+
+    printf "\nHere are your values as maccel understands them now:\n"
+
+    print_bold "SENS MULT:  "
+    maccel get sens-mult
+
+    print_bold "ACCEL:      "
+    maccel get accel
+
+    print_bold "OFFSET:     "
+    maccel get offset
+
+    print_bold "OUTPUT CAP: "
+    maccel get output-cap
+  fi 
+}
+
 install_driver() {
   make uninstall || true
   if [ $MACCEL_DEBUG_INSTALL -eq 1 ]; then
@@ -36,6 +77,7 @@ trigger_udev_rules() {
 	udevadm trigger --subsystem-match=usb --subsystem-match=input --subsystem-match=hid --attr-match=bInterfaceClass=03 --attr-match=bInterfaceSubClass=01 --attr-match=bInterfaceProtocol=02
 }
 
+version_update_warning
 
 # Run the installation
 setup_dirs
