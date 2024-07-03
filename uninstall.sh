488bac2327d43f6d3f40fab23cf0d0a58ae0435c
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-07-03 11:21:36 -0400

    chore: fix lints, and making sure we unbind mice before removing kernel module during install

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,15 @@
-MODULEDIR=/lib/modules/`uname -r`/kernel/drivers/usb
+MODULEDIR=/lib/modules/$(uname -r)/kernel/drivers/usb
 
 delete_module() {
-	sudo rmmod maccel
-	sudo rm -vf $MODULEDIR/maccel.ko
+  sudo rmmod maccel
+  sudo rm -vf $MODULEDIR/maccel.ko
 }
 
 udev_uninstall() {
-	sudo maccel unbindall
+  sudo maccel unbindall
 
-	sudo rm -vf /usr/lib/udev/rules.d/99-maccel.rules /usr/lib/udev/maccel_bind
-	sudo udevadm control --reload-rules
+  sudo rm -vf /usr/lib/udev/rules.d/99-maccel.rules /usr/lib/udev/maccel_bind
+  sudo udevadm control --reload-rules
 }
 
 delete_everything() {
@@ -17,6 +17,6 @@ delete_everything() {
   sudo rm -vrf /opt/maccel /var/opt/maccel
 }
 
-delete_module
 udev_uninstall
+delete_module
 delete_everything
