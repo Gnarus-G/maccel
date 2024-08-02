e3031e3f1c7e887406c3f7ac1c12199898835181
gnarus-g <g.avet.gnaritas@gmail.com>
2024-08-01 20:39:07 -0400

    experiment: maccel as an input handler

--- driver/maccel.c
+++ driver/maccel.c
@@ -1,10 +1,9 @@
 #include "accel.h"
 #include "dbg.h"
-#include "linux/input-event-codes.h"
 #include "linux/ktime.h"
-#include "linux/mod_devicetable.h"
 #include "params.h"
 #include <linux/hid.h>
+#include <linux/input.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
@@ -13,10 +12,6 @@ MODULE_DESCRIPTION("Mouse acceleration driver");
 
 #define USB_VENDOR_ID_RAZER 0x1532
 #define USB_VENDOR_ID_RAZER_VIPER 0x0078
-
-static bool maccel_match(struct hid_device *dev, bool ignore_special_driver) {
-  return dev->type == HID_TYPE_USBMOUSE;
-}
 
 static AccelResult inline accelerate(s8 x, s8 y) {
   static ktime_t last;
@@ -41,58 +36,88 @@ static AccelResult inline accelerate(s8 x, s8 y) {
                       PARAM_OUTPUT_CAP);
 }
 
-static int maccel_event(struct hid_device *hdev, struct hid_field *field,
-                        struct hid_usage *usage, __s32 value) {
-  static s8 x_value = 0;
-  static s8 y_value = 0;
+static bool my_input_filter(struct input_handle *handle, unsigned int type,
+                            unsigned int code, int value) {
+  static int x_value = 0;
+  static int y_value = 0;
 
-  if (usage->type == EV_REL) {
-    hid_info(hdev, "Event: mouse movement");
-    AccelResult result = accelerate(x_value, y_value);
+  if (type == EV_REL) {
+    AccelResult accelerated = accelerate(x_value, y_value);
 
-    if (usage->code == REL_X) {
+    if (code == REL_X) {
       x_value = value;
-      hid_info(hdev, "Event: mouse move x value: %d", *field->new_value);
-      hid_info(hdev, "Event: mouse move x value: %d", value);
-
-      input_event(field->hidinput->input, usage->type, usage->code, result.x);
+      input_event(handle->dev, type, code, accelerated.x);
     }
 
-    if (usage->code == REL_Y) {
+    if (code == REL_Y) {
       y_value = value;
-      hid_info(hdev, "Event: mouse move y value: %d", *field->new_value);
-      hid_info(hdev, "Event: mouse move y value: %d", value);
-      input_event(field->hidinput->input, usage->type, usage->code, result.y);
+      input_event(handle->dev, type, code, accelerated.y);
     }
 
-    return 1;
+    return true; // since we handled this skip the following event handlers.
+  }
+
+  return false;
+}
+
+static int my_input_connect(struct input_handler *handler,
+                            struct input_dev *dev,
+                            const struct input_device_id *id) {
+  struct input_handle *handle;
+  int error;
+
+  handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+  if (!handle)
+    return -ENOMEM;
+
+  handle->dev = input_get_device(dev);
+  handle->handler = handler;
+  handle->name = "maccel";
+
+  error = input_register_handle(handle);
+  if (error) {
+    kfree(handle);
+    return error;
+  }
+
+  error = input_open_device(handle);
+  if (error) {
+    input_unregister_handle(handle);
+    kfree(handle);
+    return error;
   }
 
   return 0;
 }
 
-static const struct hid_device_id maccel_devices[] = {
-    {HID_USB_DEVICE(USB_VENDOR_ID_RAZER, USB_VENDOR_ID_RAZER_VIPER)},
-    {HID_USB_DEVICE(0x258a, 0x0036)}, // Glorious Medel 0
-    {}};
+static void my_input_disconnect(struct input_handle *handle) {
+  input_close_device(handle);
+  input_unregister_handle(handle);
+  kfree(handle);
+}
 
-/* static const struct hid_device_id maccel_devices[] = { */
-/*     {HID_USB_DEVICE(HID_ANY_ID, HID_ANY_ID)}, {}}; */
-
-static const struct hid_usage_id maccel_usages[] = {
-    {HID_USAGE_ID(HID_GD_X, EV_REL, REL_X)},
-    {HID_USAGE_ID(HID_GD_Y, EV_REL, REL_Y)},
-    /* {HID_USAGE_ID(HID_ANY_ID, HID_ANY_ID, HID_ANY_ID)}, */
-    {HID_TERMINATOR, HID_TERMINATOR, HID_TERMINATOR}};
-
-MODULE_DEVICE_TABLE(hid, maccel_devices);
-
-static struct hid_driver maccel_driver = {
-    .name = "maccel",
-    .id_table = maccel_devices,
-    .usage_table = maccel_usages,
-    .event = maccel_event,
-    .match = maccel_match,
+static const struct input_device_id my_ids[] = {
+    {.driver_info = 1}, // Match all devices
+    {},
 };
 
-module_hid_driver(maccel_driver);
+MODULE_DEVICE_TABLE(input, my_ids);
+
+static struct input_handler my_input_handler = {
+    .filter = my_input_filter,
+    .connect = my_input_connect,
+    .disconnect = my_input_disconnect,
+    .name = "maccel",
+    .id_table = my_ids,
+};
+
+static int __init my_init(void) {
+  return input_register_handler(&my_input_handler);
+}
+
+static void __exit my_exit(void) {
+  input_unregister_handler(&my_input_handler);
+}
+
+module_init(my_init);
+module_exit(my_exit);
