3883c03293ba02f69ebbfb519ac925a9fddf41ba
Alexander Van Damme <vandammea06@gmail.com>
2024-08-11 21:30:41 +0300

    Added some custom acceleration functions to test with.

--- driver/accel.h
+++ driver/accel.h
@@ -4,6 +4,29 @@
 #include "dbg.h"
 #include "fixedptc.h"
 
+// Linear profile
+static inline fixedpt linear_profile(fixedpt param_accel, fixedpt input_speed) {
+  return fixedpt_add(FIXEDPT_ONE, fixedpt_mul((param_accel), input_speed));
+}
+
+// Logarithmic profile, didn't manage to test it
+static inline fixedpt log_profile(fixedpt input_speed) {
+  // Logarithmic curve (base can be adjusted)
+  return fixedpt_ln(fixedpt_add(FIXEDPT_ONE, input_speed)); // log(1 + input_speed)
+}
+
+// Sigmoid profile, tested it, didn't behave as expetced
+static inline fixedpt sigmoid_profile(fixedpt input_speed) {
+  // Sigmoid curve: 1 / (1 + exp(-input_speed))
+  fixedpt exp_input_speed = fixedpt_exp(input_speed);
+  return fixedpt_div(FIXEDPT_ONE, fixedpt_add(FIXEDPT_ONE, exp_input_speed));
+}
+
+// Kinda motivity, didn't manage to test it yet
+static inline fixedpt alisk_motivity_profile(fixedpt input_speed) {
+  return fixedpt_add(FIXEDPT_ONE, fixedpt_div(fixedpt_sub(fixedpt_exp(FIXEDPT_ONE), FIXEDPT_ONE), fixedpt_add(FIXEDPT_ONE, fixedpt_exp(fixedpt_sub(fixedpt_mul(FIXEDPT_CONST(-0.4), input_speed), FIXEDPT_CONST(-6.0))))));
+}
+
 typedef unsigned int u32;
 
 typedef struct {
@@ -29,7 +52,17 @@ extern inline fixedpt sensitivity(fixedpt input_speed, fixedpt param_sens_mult,
   dbg("accel    %s", fixedpt_cstr(param_accel, 6));
 
   if (input_speed > FIXEDPT_ZERO) {
-    sens = fixedpt_add(FIXEDPT_ONE, fixedpt_mul((param_accel), input_speed));
+    // Use linear acceleration
+    // sens = linear_profile(param_accel, input_speed);
+
+    // Use a logarithmic profile
+    // sens = log_profile(input_speed);
+
+    // Or use a sigmoid profile instead
+    // sens = sigmoid_profile(input_speed);
+
+    // Use motivity like function 1+ ((e-1)/(1+e^-(0.4*x-6))) (see: https://www.desmos.com/calculator  input: y=\frac{e-1}{1+e^{-\left(0.4x-6\right)}}+1)
+    sens = alisk_motivity_profile(input_speed);
   }
 
   sens = fixedpt_mul(sens, param_sens_mult);
