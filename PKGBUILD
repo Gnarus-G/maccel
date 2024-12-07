Gnarus-G/maccel

commit 5170be7ac87fb290aa18cc393b8d690d19c2a22d

Move dkms.conf to driver directory

PKGBUILD
@@ -24,15 +24,12 @@ package() {
     install -Dm644 ${srcdir}/maccel/maccel.sysusers ${pkgdir}/usr/lib/sysusers.d/${_pkgname}.conf
 
     # Install Driver
-    install -Dm644 ${srcdir}/maccel/dkms.conf ${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf
+    install -Dm644 ${srcdir}/maccel/driver/* ${pkgdir}/usr/src/${_pkgname}-${pkgver}/
 
     # Set name and version
     sed -e "s/@_PKGNAME@/${_pkgname}/" \
         -e "s/@PKGVER@/${pkgver}/" \
         -i "${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf"
-
-    # Copy sources (including Makefile)
-    cp -r ${srcdir}/maccel/driver/* ${pkgdir}/usr/src/${_pkgname}-${pkgver}/
 
     # Install CLI
     install -Dm 755 $srcdir/maccel/cli/target/release/maccel ${pkgdir}/usr/local/bin/maccel

commit 86ca51d47c28e628c67b1fe1892ef835c5709b82

Fix regression caused by 5170be7

PKGBUILD
@@ -24,13 +24,14 @@ package() {
     install -Dm644 ${srcdir}/maccel/maccel.sysusers ${pkgdir}/usr/lib/sysusers.d/${_pkgname}.conf
 
     # Install Driver
-    install -dm644 ${pkgdir}/usr/src/${_pkgname}-${pkgver}
-    cp -r ${srcdir}/maccel/driver/* ${pkgdir}/usr/src/${_pkgname}-${pkgver}
+    install -Dm644 ${srcdir}/maccel/dkms.conf ${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf
 
     # Set name and version
     sed -e "s/@_PKGNAME@/${_pkgname}/" \
         -e "s/@PKGVER@/${pkgver}/" \
         -i "${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf"
+    
+    cp -r ${srcdir}/maccel/driver/* ${pkgdir}/usr/src/${_pkgname}-${pkgver}/
 
     # Install CLI
     install -Dm 755 ${srcdir}/maccel/cli/target/release/maccel ${pkgdir}/usr/bin/maccel