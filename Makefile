Gnarus-G/maccel

commit 5170be7ac87fb290aa18cc393b8d690d19c2a22d

Move dkms.conf to driver directory

PKGBUILD
@@ -24,15 +24,12 @@ package() {
     install -Dm644 ${srcdir}/maccel/maccel.sysusers ${pkgdir}/usr/lib/sysusers.d/${_pkgname}.conf
 
     # Install Driver
-    install -Dm644 ${srcdir}/maccel/dkms.conf ${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf
+    install -Dm644 ${srcdir}/maccel/driver/* ${pkgdir}/usr/src/${_pkgname}-${pkgver}/
 
     # Set name and version
     sed -e "s/@_PKGNAME@/${_pkgname}/" \
         -e "s/@PKGVER@/${pkgver}/" \
         -i "${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf"
-
-    # Copy sources (including Makefile)
-    cp -r ${srcdir}/maccel/driver/* ${pkgdir}/usr/src/${_pkgname}-${pkgver}/
 
     # Install CLI
     install -Dm 755 $srcdir/maccel/cli/target/release/maccel ${pkgdir}/usr/local/bin/maccel

commit 66e278d37a310b1aca2f7ef98d75884adf122dc5

Implement PKGBUILD and DKMS to make it for easier install
* Changes to driver Makefile to allow for DKMS

* Add config file for dkms

* Add PKGBUILD for ease of installation

Remove support for ver < 0.1.5 in PKGBUILD

Remove sudo from .install file

Update .install to account for upgrades

* Add maccel group via sysusers for PKGBUILD

* Add udev rule to trigger on module load

* Fix main Makefile due to recent changes

* Move dkms.conf to driver directory

* Add support for dkms in install script

* Fix install location of PKGBUILD and minor changes

* Fix typo in PKGBUILD

* Fix regression caused by 5170be7

* Change version number to exclude 'v' in install script

* Fix install script

* Refine install messages for PKGBUILD

* Remove modprobe from dkms install script

Add ensurance message to install script

* Add dependencies to README.md

* Add quotes to paths

* Add release with debug build mode

* Add support for debug in PKGBUILD

* Remove dead function in uninstall script

---------

Co-authored-by: Ghislain D. <g.avet.gnaritas@gmail.com>

Makefile
@@ -4,7 +4,6 @@ else
 	export LLVM=1
 endif
 DRIVERDIR?=`pwd`/driver
-KDIR?=/lib/modules/`uname -r`/build
 
 MODULEDIR?=/lib/modules/`uname -r`/kernel/drivers/usb
 
@@ -14,7 +13,7 @@ debug: EXTRA_CFLAGS := -DDEBUG
 debug: default
 
 build:
-	$(MAKE) CC=$(CC) EXTRA_CFLAGS=$(EXTRA_CFLAGS) -C $(KDIR) M=$(DRIVERDIR)
+	$(MAKE) -C $(DRIVERDIR)
 
 debug_install: debug install
 
@@ -66,6 +65,4 @@ udev_trigger:
 	udevadm trigger --subsystem-match=usb --subsystem-match=input --subsystem-match=hid --attr-match=bInterfaceClass=03 --attr-match=bInterfaceSubClass=01 --attr-match=bInterfaceProtocol=02
 
 clean:
-	rm -rf $(DRIVERDIR)/.*.cmd $(DRIVERDIR)/*.ko $(DRIVERDIR)/*.mod $(DRIVERDIR)/*.mod.* $(DRIVERDIR)/*.symvers $(DRIVERDIR)/*.order $(DRIVERDIR)/*.o
-	cargo clean --manifest-path=cli/Cargo.toml
-	cargo clean --manifest-path=cli/usbmouse/Cargo.toml
+	$(MAKE) -C $(DRIVERDIR) clean

PKGBUILD
@@ -1,0 +1,59 @@
+pkgname=maccel-dkms
+_pkgname="maccel"
+pkgver=0.2.1
+pkgrel=1
+pkgdesc="Mouse acceleration driver and kernel module for Linux."
+arch=("x86_64")
+url="https://www.maccel.org/"
+license=("GPL-2.0-or-later")
+
+install=maccel.install
+depends=("dkms")
+makedepends=("git" "cargo")
+
+source=("git+https://github.com/Gnarus-G/maccel.git")
+sha256sums=("SKIP")
+
+prepare() {
+    export RUSTUP_TOOLCHAIN=stable
+    
+    platform="$(rustc -vV | sed -n 's/host: //p')"
+    
+    cargo fetch --locked --target "${platform}" --manifest-path="${srcdir}/maccel/cli/Cargo.toml"
+    cargo fetch --locked --target "${platform}" --manifest-path="${srcdir}/maccel/cli/usbmouse/Cargo.toml"
+}
+
+build() {
+    export RUSTUP_TOOLCHAIN=stable
+    export CARGO_TARGET_DIR=target
+    
+    # Build the CLI
+	  cargo build --profile=release-with-debug --frozen --all-features --manifest-path="${srcdir}/maccel/cli/Cargo.toml"
+	  cargo build --profile=release-with-debug --frozen --all-features --manifest-path="${srcdir}/maccel/cli/usbmouse/Cargo.toml"
+}
+
+package() {
+    # Add group
+    install -Dm 644 "${srcdir}/maccel/maccel.sysusers" "${pkgdir}/usr/lib/sysusers.d/${_pkgname}.conf"
+
+    # Install Driver
+    install -Dm 644 "${srcdir}/maccel/dkms.conf" "${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf"
+
+    # Set name and version
+    sed -e "s/@_PKGNAME@/${_pkgname}/" \
+        -e "s/@PKGVER@/${pkgver}/" \
+        -i "${pkgdir}/usr/src/${_pkgname}-${pkgver}/dkms.conf"
+    
+    cp -r "${srcdir}/maccel/driver/." "${pkgdir}/usr/src/${_pkgname}-${pkgver}/"
+
+    # Install CLI
+    install -Dm 755 "${srcdir}/target/release-with-debug/maccel" "${pkgdir}/usr/bin/maccel"
+    install -Dm 755 "${srcdir}/target/release-with-debug/maccel-driver-binder" "${pkgdir}/usr/bin/maccel-driver-binder"
+
+    # Install udev rules
+    install -Dm 644 "${srcdir}/maccel/udev_rules/99-maccel.rules" "${pkgdir}/usr/lib/udev/rules.d/99-maccel.rules"
+    install -Dm 755 "${srcdir}/maccel/udev_rules/maccel_param_ownership_and_resets" "${pkgdir}/usr/lib/udev/maccel_param_ownership_and_resets"
+    
+    # Install License
+    install -Dm 644 "${srcdir}/maccel/LICENSE" "${pkgdir}/usr/share/licenses/${_pkgname}/LICENSE"
+}

cli/Cargo.toml
@@ -3,6 +3,10 @@ name = "maccel-cli"
 version = "0.2.1"
 edition = "2021"
 
+[profile.release-with-debug]
+inherits = "release"
+debug = true
+
 [[bin]]
 name = "maccel"
 path = "src/main.rs"

cli/usbmouse/Cargo.toml
@@ -3,6 +3,10 @@ name = "usbmouse-cli"
 version = "0.1.0"
 edition = "2021"
 
+[profile.release-with-debug]
+inherits = "release"
+debug = true
+
 [[bin]]
 name = "maccel-driver-binder"
 path = "src/main.rs"

dkms.conf
@@ -1,0 +1,7 @@
+PACKAGE_NAME="@_PKGNAME@"
+PACKAGE_VERSION="@PKGVER@"
+MAKE[0]="make KVER=$kernelver"
+CLEAN="make clean"
+BUILT_MODULE_NAME[0]="@_PKGNAME@"
+DEST_MODULE_LOCATION[0]="/kernel/drivers/usb"
+AUTOINSTALL="yes"

driver/Makefile
@@ -1,5 +1,14 @@
 obj-m += maccel.o
 
+KVER ?= $(shell uname -r)
+KDIR ?= /lib/modules/$(KVER)/build
+
+build:
+		$(MAKE) -C $(KDIR) M=$(CURDIR)
+
+clean:
+		$(MAKE) -C $(KDIR) M=$(CURDIR) clean
+
 test:
 	@mkdir -p tests/snapshots
 	@gcc tests/test.c -o accel_test -g

install.sh
@@ -40,7 +40,12 @@ get_current_version(){
   maccel -V | awk '{ print $2 }'
 }
 
+get_version() {
+  wget -qO- https://github.com/Gnarus-G/maccel/releases/latest | grep -oP 'v\d+\.\d+\.\d+' | tail -n 1 | cut -c 2-
+}
+
 CURR_VERSION=$(get_current_version)
+VERSION=$(get_version)
 
 set -e
 
@@ -93,12 +98,30 @@ install_driver() {
   fi
 }
 
+install_driver_dkms() {  
+    # Install Driver 
+    install -Dm 644 "$(pwd)/dkms.conf" "/usr/src/maccel-${VERSION}/dkms.conf"
+    
+    # Set name and version
+    sudo sed -e "s/@_PKGNAME@/maccel/" \
+         -e "s/@PKGVER@/${VERSION}/" \
+         -i "/usr/src/maccel-${VERSION}/dkms.conf"
+    
+    sudo cp -r "$(pwd)/driver/." "/usr/src/maccel-${VERSION}/"
+
+    sudo dkms install "maccel/${VERSION}"
+
+    print_bold $(print_green "[Recommended]")
+    print_bold ' Make sure to run `modprobe maccel` after install\n'
+    print_bold $(print_green "[Recommended]")
+    print_bold ' unless you have `modprobe_on_install` added to your dkms config.\n'
+}
+
 install_cli() {
-  VERSION=$(wget -qO- https://github.com/Gnarus-G/maccel/releases/latest | grep -oP 'v\d+\.\d+\.\d+' | tail -n 1)
-  curl -fsSL https://github.com/Gnarus-G/maccel/releases/download/$VERSION/maccel-cli.tar.gz -o maccel-cli.tar.gz
-  tar -zxvf maccel-cli.tar.gz maccel_$VERSION/maccel
+  curl -fsSL https://github.com/Gnarus-G/maccel/releases/download/v$VERSION/maccel-cli.tar.gz -o maccel-cli.tar.gz
+  tar -zxvf maccel-cli.tar.gz maccel_v$VERSION/maccel
   mkdir -p bin
-  sudo install -m 755 -v -D maccel_$VERSION/maccel* bin/
+  sudo install -m 755 -v -D maccel_v$VERSION/maccel* bin/
   sudo ln -vfs $(pwd)/bin/maccel* /usr/local/bin/
 }
 
@@ -134,7 +157,8 @@ underline_start
 print_bold "\nInstalling the driver (kernel module)"
 underline_end
 
-install_driver
+#install_driver
+install_driver_dkms
 
 underline_start
 print_bold "\nInstalling the CLI"

maccel.install
@@ -1,0 +1,25 @@
+post_install() {
+    udevadm control --reload-rules
+
+    echo ''
+    echo ' Add yourself to the "maccel" group and install the module!'
+    echo '   usermod -aG maccel $USER && modprobe maccel'
+    echo ''
+}
+
+post_upgrade() {
+    udevadm control --reload-rules
+    
+    echo ''
+    echo ' Remember to reload the module!'
+    echo '   rmmod maccel && modprobe maccel'
+    echo ''
+}
+
+pre_remove() {
+    maccel driver unbindall
+}
+
+post_remove() {
+    udevadm control --reload-rules
+}

maccel.sysusers
@@ -1,0 +1,1 @@
+g maccel - -

udev_rules/99-maccel.rules
@@ -1,3 +1,5 @@
+ACTION=="add", SUBSYSTEM=="module", DEVPATH="/module/maccel", RUN+="maccel_param_ownership_and_resets"
+
 ACTION=="remove", GOTO="maccel_end"
 SUBSYSTEMS=="usb|input|hid", ATTRS{bInterfaceClass}=="03", ATTRS{bInterfaceSubClass}=="01", ATTRS{bInterfaceProtocol}=="02", RUN+="maccel_param_ownership_and_resets"
 

uninstall.sh
@@ -10,9 +10,10 @@ get_current_version(){
 
 CURR_VERSION=$(get_current_version)
 
-delete_module() {
+delete_module_dkms() {
   sudo rmmod maccel
-  sudo rm -vf $MODULEDIR/maccel.ko
+  sudo dkms remove maccel/${CURR_VERSION}
+  sudo rm -rfv /usr/src/maccel-${CURR_VERSION}
 }
 
 udev_uninstall() {
@@ -32,5 +33,5 @@ delete_everything() {
 }
 
 udev_uninstall
-delete_module
+delete_module_dkms
 delete_everything