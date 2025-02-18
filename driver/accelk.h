b13759bb12c30a2a8f401439518e0bbac53abfa5
gnarus-g <g.avet.gnaritas@gmail.com>
2025-02-17 21:55:03 -0500

    Cap polling rate support as up to 8kHz

--- driver/accelk.h
+++ driver/accelk.h
@@ -2,24 +2,32 @@
 #define _ACCELK_H_
 
 #include "accel.h"
+#include "fixedptc.h"
 #include "linux/ktime.h"
 #include "params.h"
 
 const fixedpt HUNDRED_MS = fixedpt_rconst(100);
 const fixedpt US_PER_MS = fixedpt_rconst(1000);
 
+const u64 EIGTH_OF_A_MS_IN_US = 125; // To support up to 8000Hz polling rate
+                                     // we should cap the floor to 0.125ms.
+                                     // This is an arbitrary decision...
+
 static inline AccelResult accelerate(int x, int y) {
   dbg("FIXEDPT_BITS = %d", FIXEDPT_BITS);
 
   static ktime_t last_time;
-  static s64 last_us = 100; // .1ms
-
+  static s64 last_us = EIGTH_OF_A_MS_IN_US;
   ktime_t now = ktime_get();
+
   s64 us = ktime_to_us(now - last_time);
+
+  dbg("ktime interval -> now (%llu) vs last_ktime (%llu), diff = %llius", now,
+      last_time, us);
 
   last_time = now;
 
-  if (us < 100) { // ensure no less than .1ms?
+  if (us < EIGTH_OF_A_MS_IN_US) { // setting a floor for safety
     us = last_us;
   }
 
@@ -27,6 +35,8 @@ static inline AccelResult accelerate(int x, int y) {
 
   fixedpt _us = fixedpt_fromint(us);
   fixedpt ms = fixedpt_div(_us, US_PER_MS);
+
+  dbg("ktime interval, converting to ms: %lluus -> %sms", us, fptoa(ms));
 
   if (ms > HUNDRED_MS) { // rounding down to 100 ms
     ms = HUNDRED_MS;
