8f1a23e25a7613f32108cb8cff787e46bba11145
gnarus-g <g.avet.gnaritas@gmail.com>
2025-02-23 00:32:46 -0500

    Enable more precision on time interval for s32.32

--- driver/accelk.h
+++ driver/accelk.h
@@ -7,43 +7,61 @@
 #include "params.h"
 
 const fixedpt HUNDRED_MS = fixedpt_rconst(100);
-const fixedpt US_PER_MS = fixedpt_rconst(1000);
 
-const u64 EIGTH_OF_A_MS_IN_US = 125; // To support up to 8000Hz polling rate
-                                     // we should cap the floor to 0.125ms.
-                                     // This is an arbitrary decision...
+// To support up to 8000Hz polling rate
+// we should cap the floor to 0.125ms.
+// This is an arbitrary decision...
+#if FIXEDPT_BITS == 64
+const s64 EIGTH_OF_A_MS_IN_UNIT = 125000; // 0.125ms in ns
+const fixedpt UNIT_PER_MS = fixedpt_rconst(1000000);
+#else
+const s64 EIGTH_OF_A_MS_IN_UNIT = 125; // 0.125ms in us
+const fixedpt UNIT_PER_MS = fixedpt_rconst(1000);
+#endif
 
 static inline AccelResult accelerate(int x, int y) {
   dbg("FIXEDPT_BITS = %d", FIXEDPT_BITS);
 
   static ktime_t last_time;
-  static s64 last_us = EIGTH_OF_A_MS_IN_US;
+  static s64 last_unit_time = EIGTH_OF_A_MS_IN_UNIT;
   ktime_t now = ktime_get();
 
-  s64 us = ktime_to_us(now - last_time);
-
+#if FIXEDPT_BITS == 64
+  s64 unit_time = ktime_to_ns(now - last_time);
+  dbg("ktime interval -> now (%llu) vs last_ktime (%llu), diff = %llins", now,
+      last_time, unit_time);
+#else
+  s64 unit_time = ktime_to_us(now - last_time);
   dbg("ktime interval -> now (%llu) vs last_ktime (%llu), diff = %llius", now,
-      last_time, us);
+      last_time, unit_time);
+#endif
 
   last_time = now;
 
-  if (us < EIGTH_OF_A_MS_IN_US) { // setting a floor for safety
-    us = last_us;
+  if (unit_time < EIGTH_OF_A_MS_IN_UNIT) { // setting a floor for safety
+    unit_time = last_unit_time;
   }
 
-  last_us = us;
+  last_unit_time = unit_time;
 
-  fixedpt _us = fixedpt_fromint(us);
-  fixedpt ms = fixedpt_div(_us, US_PER_MS);
+  fixedpt _unit_time = fixedpt_fromint(unit_time);
+  fixedpt millisecond = fixedpt_div(_unit_time, UNIT_PER_MS);
 
-  dbg("ktime interval, converting to ms: %lluus -> %sms", us, fptoa(ms));
+#if FIXEDPT_BITS == 64
+  dbg("ktime interval, converting to ns: %lluns -> %sms", unit_time,
+      fptoa(millisecond));
+#else
+  dbg("ktime interval, converting to us: %lluus -> %sms", unit_time,
+      fptoa(millisecond));
+#endif
 
-  if (ms > HUNDRED_MS) { // rounding down to 100 ms
-    ms = HUNDRED_MS;
+  if (millisecond > HUNDRED_MS) { // rounding down to 100 ms
+    millisecond = HUNDRED_MS;
   }
 
-  return f_accelerate(x, y, ms, atofp(PARAM_SENS_MULT), atofp(PARAM_ACCEL),
-                      atofp(PARAM_OFFSET), atofp(PARAM_OUTPUT_CAP));
+  return f_accelerate(x, y, millisecond, atofp(PARAM_SENS_MULT),
+                      atofp(PARAM_ACCEL), atofp(PARAM_OFFSET),
+                      atofp(PARAM_OUTPUT_CAP));
 }
 
 #endif // !_ACCELK_H_
