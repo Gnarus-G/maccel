8b3937c726b2bb4504dda110ef39058e78e49992
gnarus-g <g.avet.gnaritas@gmail.com>
2025-02-23 00:37:37 -0500

    Remove unecessary code when get time interval

--- driver/accelk.h
+++ driver/accelk.h
@@ -6,24 +6,16 @@
 #include "linux/ktime.h"
 #include "params.h"
 
-const fixedpt HUNDRED_MS = fixedpt_rconst(100);
-
-// To support up to 8000Hz polling rate
-// we should cap the floor to 0.125ms.
-// This is an arbitrary decision...
 #if FIXEDPT_BITS == 64
-const s64 EIGTH_OF_A_MS_IN_UNIT = 125000; // 0.125ms in ns
-const fixedpt UNIT_PER_MS = fixedpt_rconst(1000000);
+const fixedpt UNIT_PER_MS = fixedpt_rconst(1000000); // 1 million nanoseconds
 #else
-const s64 EIGTH_OF_A_MS_IN_UNIT = 125; // 0.125ms in us
-const fixedpt UNIT_PER_MS = fixedpt_rconst(1000);
+const fixedpt UNIT_PER_MS = fixedpt_rconst(1000); // 1 thousand microsends
 #endif
 
 static inline AccelResult accelerate(int x, int y) {
   dbg("FIXEDPT_BITS = %d", FIXEDPT_BITS);
 
   static ktime_t last_time;
-  static s64 last_unit_time = EIGTH_OF_A_MS_IN_UNIT;
   ktime_t now = ktime_get();
 
 #if FIXEDPT_BITS == 64
@@ -35,29 +27,18 @@ static inline AccelResult accelerate(int x, int y) {
   dbg("ktime interval -> now (%llu) vs last_ktime (%llu), diff = %llius", now,
       last_time, unit_time);
 #endif
-
   last_time = now;
-
-  if (unit_time < EIGTH_OF_A_MS_IN_UNIT) { // setting a floor for safety
-    unit_time = last_unit_time;
-  }
-
-  last_unit_time = unit_time;
 
   fixedpt _unit_time = fixedpt_fromint(unit_time);
   fixedpt millisecond = fixedpt_div(_unit_time, UNIT_PER_MS);
 
 #if FIXEDPT_BITS == 64
-  dbg("ktime interval, converting to ns: %lluns -> %sms", unit_time,
+  dbg("ktime interval -> converting to ns: %lluns -> %sms", unit_time,
       fptoa(millisecond));
 #else
   dbg("ktime interval, converting to us: %lluus -> %sms", unit_time,
       fptoa(millisecond));
 #endif
-
-  if (millisecond > HUNDRED_MS) { // rounding down to 100 ms
-    millisecond = HUNDRED_MS;
-  }
 
   return f_accelerate(x, y, millisecond, atofp(PARAM_SENS_MULT),
                       atofp(PARAM_ACCEL), atofp(PARAM_OFFSET),
