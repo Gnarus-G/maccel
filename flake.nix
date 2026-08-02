befb749c37eba85fb6d0be9711cf27fe3a5e098c
gnarus-g <g.avet.gnaritas@gmail.com>
2026-08-01 21:13:47 -0400

    Expose default Nix package

--- flake.nix
+++ flake.nix
@@ -1,7 +1,44 @@
 {
-  description = "maccel NixOS Module";
+  description = "maccel mouse acceleration driver and CLI";
 
-  outputs = {...}: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    moduleEvaluation = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        self.nixosModules.default
+        {
+          hardware.maccel = {
+            enable = true;
+            enableCli = true;
+          };
+          system.stateVersion = "24.11";
+        }
+      ];
+    };
+  in {
+    packages.${system} = {
+      maccel-cli = pkgs.callPackage ./package.nix {};
+      default = self.packages.${system}.maccel-cli;
+    };
+    checks.${system} = {
+      default = self.packages.${system}.default;
+      nixos-module = let
+        installedPackages = map nixpkgs.lib.getName moduleEvaluation.config.environment.systemPackages;
+      in
+        assert builtins.elem "maccel-cli" installedPackages;
+          pkgs.runCommand "maccel-module-check" {} ''
+            touch $out
+          '';
+    };
+
     nixosModules.default = import ./module.nix;
   };
 }
